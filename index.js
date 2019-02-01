27b6e6ae15c1e0a12eefc89c6914336dc6af639b
guoxuemin <469477762@qq.com>
2019-02-01 18:13:43 +0800

    test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
 'use strict'
 
 function createRequireConfig(ret, conf, settings, opt) {
     const DOMAIN = settings.domain || ''
     const USE_COMBO = settings.combo
+    const inlineFirstScreenCss = settings.inlineFirstScreenCss
     const COMBO_URL = settings.comboUrl || '/??'
 
     var _ = fis.util
@@ -57,8 +57,8 @@ function createRequireConfig(ret, conf, settings, opt) {
     }
     var getId = function(_file) {
         let file = res[_file];
-         if(!file){
-            fis.log.error("模块["+_file+"]不存在！")
+        if (!file) {
+            fis.log.error("模块[" + _file + "]不存在！")
         }
         return (file.extras && file.extras.moduleId) ? file.extras.moduleId : (idsObj[_file] ? (idsObj[_file].moduleId || _file) : _file)
     }
@@ -87,6 +87,37 @@ function createRequireConfig(ret, conf, settings, opt) {
         })
         if (deps === void(0)) return rz;
     }
+
+    // 将首屏（isFirstScreen）模块的依赖也加上 isFirstScreen 标示
+
+    function addDepsIsFirstScreen(deps) {
+        deps.forEach((file, index) => {
+            if (res[file].extras) {
+                res[file].extras.isFirstScreen = true;
+            } else {
+                res[file].extras = {
+                    isFirstScreen: true
+                }
+            }
+
+            if (res[file].deps) {
+                addDepsIsFirstScreen(res[file].deps);
+            }
+        })
+    }
+
+    files.forEach(function(_file, i) {
+        const extras = res[_file].extras;
+        if (extras && extras.isFirstScreen) {
+
+            const deps = res[_file].deps || [];
+            addDepsIsFirstScreen(deps);
+        }
+
+
+    })
+
+
     // console.log(files,'files')
     files.forEach(function(_file, i) {
         let _allDeps = []
@@ -119,18 +150,21 @@ function createRequireConfig(ret, conf, settings, opt) {
                 let scripts = comboUrl.scripts[_file]
                 let type = res[__file] ? res[__file].type : ''
                 let cType = (res[__file] && res[__file].extras) ? res[__file].extras.comboTo : void 0
+                let isFirstScreen = (res[__file] && res[__file].extras) ? res[__file].extras.isFirstScreen : false;
                 let cOrder = (res[__file] && res[__file].extras) ? res[__file].extras.comboOrder : 1
                 if (type === 'css') {
                     if (!links) {
                         comboUrl.links[_file] = [{
                             url: getUri(__file),
                             cType: cType,
+                            isFirstScreen,
                             cOrder: cOrder
                         }]
                     } else {
                         links.push({
                             url: getUri(__file),
                             cType: cType,
+                            isFirstScreen,
                             cOrder: cOrder
                         })
                     }
@@ -187,9 +221,9 @@ function createRequireConfig(ret, conf, settings, opt) {
                 let file = res[_file]
                 let id = getId(_file)
                 let type = file.type
-                if(type === 'css'){
-                  thisPath[_file] = allModulePath[_file]
-                }else 
+                if (type === 'css') {
+                    thisPath[_file] = allModulePath[_file]
+                } else
                 if (type === 'js') {
                     thisPath[id] = allModulePath[_file]
                 }
@@ -216,6 +250,7 @@ function createRequireConfig(ret, conf, settings, opt) {
             let comboJS = {}
             let comboCSS = {}
 
+            let firstScreen = []
             let comboJSRz = []
             let comboCSSRz = []
             let unComboJSRzTop = []
@@ -228,7 +263,10 @@ function createRequireConfig(ret, conf, settings, opt) {
                 comboJS[type] ? comboJS[type].push(obj) : comboJS[type] = [obj]
             })
             _comboCSS.forEach(function(obj) {
-                let type = obj.cType || -99
+                let type = obj.cType || -99;
+                if (obj.isFirstScreen) {
+                    type = 88888888;
+                }
                 comboCSS[type] ? comboCSS[type].push(obj) : comboCSS[type] = [obj]
             })
             Object.keys(comboJS).map(function(type) {
@@ -259,89 +297,62 @@ function createRequireConfig(ret, conf, settings, opt) {
             Object.keys(comboCSS).map(function(type) {
                 return +type
             }).sort().forEach(function(type) {
-                let thisComboCSS = comboCSS['' + type]
-                // thisComboCSS.reverse()
-                if (type <= 99 && type >= 0 && oConfig.combo) {
-                    let urls = thisComboCSS.sort(function(a, b) {
-                        return a.cOrder - b.cOrder
-                    }).map(function(obj) {
-                        return obj.url.replace(DOMAIN, '')
-                    })
-                    comboCSSRz.push('<link href="' + DOMAIN + COMBO_URL + urls.join(',') + '" type="text/css" rel="stylesheet" />')
-                    // ;[].push.apply(comboCSSRz,thisComboCSS.map(function(url){return url.replace(DOMAIN , '')}))
-                } else if (type < 0) {
-                    unComboCSSRzTop.push(thisComboCSS.map(function(obj) {
-                        return '<link href="' + DOMAIN + obj.url + '" type="text/css" rel="stylesheet" />'
-                    }).join('\n'))
+                let thisComboCSS = comboCSS['' + type];
+
+                // 首屏
+                if (type == 88888888) {
+
+                    const inline = inlineFirstScreenCss ? '__inline="true"' : '';
+
+                    if (oConfig.combo) {
+                        let urls = thisComboCSS.sort(function(a, b) {
+                            return a.cOrder - b.cOrder
+                        }).map(function(obj) {
+
+                            return obj.url.replace(DOMAIN, '')
+                        })
+
+
+                        firstScreen.push('<link ' + inline + ' href="' + DOMAIN + COMBO_URL + urls.join(',') + '" type="text/css" rel="stylesheet" first="true" />')
+
+                    } else {
+                        firstScreen.push(thisComboCSS.map(function(obj) {
+                            return '<link ' + inline + ' href="' + DOMAIN + obj.url + '" type="text/css" rel="stylesheet" first="true" />'
+                        }).join('\n'))
+                    }
+
+
+
                 } else {
-                    unComboCSSRzLaster.push(thisComboCSS.map(function(obj) {
-                        return '<link href="' + DOMAIN + obj.url + '" type="text/css" rel="stylesheet" />'
-                    }).join('\n'))
+
+                    // thisComboCSS.reverse()
+                    if (type <= 99 && type >= 0 && oConfig.combo) {
+                        let urls = thisComboCSS.sort(function(a, b) {
+                            return a.cOrder - b.cOrder
+                        }).map(function(obj) {
+                            return obj.url.replace(DOMAIN, '')
+                        })
+                        comboCSSRz.push('<link href="' + DOMAIN + COMBO_URL + urls.join(',') + '" type="text/css" rel="stylesheet" />')
+                        // ;[].push.apply(comboCSSRz,thisComboCSS.map(function(url){return url.replace(DOMAIN , '')}))
+                    } else if (type < 0) {
+                        unComboCSSRzTop.push(thisComboCSS.map(function(obj) {
+                            return '<link href="' + DOMAIN + obj.url + '" type="text/css" rel="stylesheet" />'
+                        }).join('\n'))
+                    } else {
+                        unComboCSSRzLaster.push(thisComboCSS.map(function(obj) {
+                            return '<link href="' + DOMAIN + obj.url + '" type="text/css" rel="stylesheet" />'
+                        }).join('\n'))
+                    }
                 }
             })
-            // Object.keys(comboJS).map(function(type){
-            //   return +type
-            // }).sort().forEach(function(type){
-            //   let thisComboJS = comboJS[''+type]
-            //   // thisComboJS.reverse()
-            //   console.log(type)
-            //   if(type <=99 && type >=0 && oConfig.combo ){
-            //     ;[].push.apply(comboJSRz,thisComboJS.map(function(url){return url.replace(DOMAIN , '')}))
-            //   }else if(type < 0){
-            //     unComboJSRzTop.push(thisComboJS.map(function(url) {
-            //         return '<script class=" defer" src="'+ DOMAIN + url + '"></script>'
-            //      }).join('\n'))
-            //   }else{
-            //     unComboJSRzLaster.push(thisComboJS.map(function(url) {
-            //         return '<script class=" defer" src="' + DOMAIN + url + '"></script>'
-            //      }).join('\n'))
-            //   }
-            // })
-            // Object.keys(comboCSS).map(function(type){
-            //   return +type
-            // }).sort().forEach(function(type){
-            //   let thisComboCSS = comboCSS[''+type]
-            //   // thisComboCSS.reverse()
-            //   if(type <=99 && type >=0 && oConfig.combo ){
-            //     ;[].push.apply(comboCSSRz,thisComboCSS.map(function(url){return url.replace(DOMAIN , '')}))
-            //   }else if(type < 0){
-            //     unComboCSSRzTop.push(thisComboCSS.map(function(url) {
-            //         return '<link type="text/css" rel="stylesheet" href="' + DOMAIN + url + '" />'
-            //      }).join('\n'))
-            //   }else if(type > 99){
-            //     unComboCSSRzLaster.push(thisComboCSS.map(function(url) {
-            //         return '<link type="text/css" rel="stylesheet" href="' + DOMAIN + url + '" />'
-            //      }).join('\n'))
-            //   }
-            // })
-            // Object.keys(comboCSS).map(function(type){
-            //   return +type
-            // }).sort().forEach(function(type){
-            //   let thisComboCSS = comboCSS[''+type]
-            //   // thisComboCSS.reverse()
-            //   let css = (type >=0 && oConfig.combo) ? '<link type="text/css" rel="stylesheet" data-order="'+type+'" href="' + oConfig.domain + oConfig.comboUrl + thisComboCSS.map(function(url){return url.replace(DOMAIN , '')}).join(',') + '"></link>' 
-            //     : (thisComboCSS.map(function(url) {
-            //         return '<link type="text/css" rel="stylesheet" href="' + url + '"></link>'
-            //      })).join('\n')
-            //   comboCSSRz.push(css)
-            // })
-            // console.log(comboJSRz,comboCSSRz)
-            // console.log(oConfig.combo)
-            // let comboScripts = comboJS ? (oConfig.combo ? '<script class=" defer" src="' + oConfig.domain + oConfig.comboUrl + comboJS.map(function(url){return url.replace(DOMAIN , '')}).join(',') + '"></script>' : (comboJS.map(function(url) {
-            //                                 return '<script class=" defer" src="' + url + '"></script>'
-            //                              })).join('\n')) : '';
-            // let comboLinks = comboCSS ? (oConfig.combo ? '<link href="' + oConfig.domain + oConfig.comboUrl + comboCSS.map(function(url){return url.replace(DOMAIN , '')}).join(',') + '" />' : (comboCSS.map(function(url) {
-            //                               return '<link href="' + url + '"/>'
-            //                            })).join('\n')) : '';
-            // console.log(comboLinks,comboCSS,_file)
-            //       console.log(_file,'=============',comboJSRz,'\n'
-            // ,unComboJSRzTop,'\n'
-            // ,unComboJSRzLaster)
+
             file.setContent(content.replace(/<!--REQUIRE_CONFIG-->|__REQUIRE_CONFIG__|window\.__REQUIRE_CONFIG__/g, config)
                 .replace('<!--COMBO_JS-->', unComboJSRzTop.join('\n') + (comboJSRz.length > 0 ? comboJSRz.join('\n') : '') + unComboJSRzLaster.join('\n'))
+                .replace('<!--FIRSTSCREEN-->', firstScreen.join('\n'))
                 .replace('<!--COMBO_CSS-->', unComboCSSRzTop.join('\n') + (comboCSSRz.length > 0 ? comboCSSRz.join('\n') : '') + unComboCSSRzLaster.join('\n')))
+
         }
 
     })
 }
-module.exports = createRequireConfig 
+module.exports = createRequireConfig
